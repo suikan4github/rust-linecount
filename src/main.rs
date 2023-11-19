9e01940303964b2861ebdcd4341521fda1270b77
Suikan <26223147+suikan4github@users.noreply.github.com>
2023-11-20 08:32:09 +0900

    Renamed sub.rs to lib.rs to follow the Rust guideline.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
-mod sub;
 use std::io::{stdin, BufReader};
 
 fn main() {
     let mut reader = BufReader::new(stdin());
 
-    let counter = sub::line_count(&mut reader);
+    let counter = rust_linecount::line_count(&mut reader);
 
     println!("{}", counter);
 }
