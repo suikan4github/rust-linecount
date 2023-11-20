cfcf1acfcb923a346cf242ca312bf7f57211ea48
Suikan <26223147+suikan4github@users.noreply.github.com>
2023-11-20 12:29:55 +0900

    Revert "Renamed sub.rs to lib.rs to follow the Rust guideline."
    This reverts commit 9e01940303964b2861ebdcd4341521fda1270b77.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
+mod sub;
 use std::io::{stdin, BufReader};
 
 fn main() {
     let mut reader = BufReader::new(stdin());
 
-    let counter = rust_linecount::line_count(&mut reader);
+    let counter = sub::line_count(&mut reader);
 
     println!("{}", counter);
 }
